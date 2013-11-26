64c59281d3b42a29031195a357849081943b61b6
bragboy <bragboy@gmail.com>
2013-11-26 19:47:23 +0530

    Trimming math override to improve coverage

--- lib/horoscope/overrides/math_override.rb
+++ lib/horoscope/overrides/math_override.rb
@@ -1,31 +1,11 @@
 # Equivalent Java methods overriden for Ruby
 module Math
-  def self.IEEEremainder(x, y)
-  	begin
-	  regularMod = Math.java_mod(x,y) #DO NOT use Ruby Mod operator here as -1%13 yields different results for Java and Ruby
-      return 0.0 if (regularMod == 0) 
-      alternativeResult = regularMod - (y.abs * Math.signum(x))
-      if alternativeResult.abs == regularMod.abs
-        divisionResult = x/y
-        roundedResult = divisionResult.round
-        return roundedResult.abs > divisionResult.abs ? alternativeResult : regularMod
-      end
-      return alternativeResult.abs < regularMod.abs ? alternativeResult : regularMod
-  	rescue ZeroDivisionError
-  	  return 0.0/0.0 #NaN
-  	end
-  end
-
   def self.toRadians(angdeg)
   	angdeg / 180.0 * Math::PI
   end
 
   def self.toDegrees(angrad)
   	angrad * 180.0 / Math::PI
-  end
-
-  def self.signum(num)
-    num > 0 ? 1 : (num == 0? 0 : -1)
   end
 
   def self.java_mod(x,y)
