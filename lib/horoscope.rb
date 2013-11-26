ea2407517ef60f9e152f706585301f7b3496eb19
bragboy <bragboy@gmail.com>
2013-11-26 10:31:19 +0530

    cleaning up files

--- lib/horoscope.rb
+++ lib/horoscope.rb
@@ -1,5 +1,71 @@
 require 'horoscope/version'
+require 'horoscope/overrides/math_override'
+require 'horoscope/planet'
+
 module Horoscope
-  
+  class Horo
+
+    PLANETS = ["As", "Su", "Mo", "Ma", "Me", "Ju", "Ve", "Sa", "Ra", "Ke"]
+
+    ERRORS = {
+      :Date => "Error: Invalid Date. Enter a valid date between years 1600 AD and 2300 AD",
+      :Lat  => "Error: Invalid Latitude. Enter between -90.0 to +90.0",
+      :Lon  => "Error: Invalid Longitude. Enter between -180.0 to +180.0"
+    }
+
+    attr_accessor :datetime, :lat, :lon, :errors, :positions
+
+    def initialize(data={})
+      @data = data
+
+      @errors = []
+
+      @datetime = data[:datetime]
+      @zone = data[:offset]
+      @lat = data[:lat]
+      @lon = data[:lon]
+      @positions = Hash[PLANETS.map {|x| [x, nil]}]
+    end
+
+    def compute
+      return @errors if validate_values.size > 0 
+
+      tpos = [10980, 16233, 15880, 16451, 15210, 13722, 13862, 7676, 4306, 15106]
+      tsp, pos, spd = [Array.new(10, 0), Array.new(10, 0), Array.new(10, 0)]
+      jd = Planet.get_jul_day(@datetime.month, @datetime.day, @datetime.year)
+      time = @datetime.hour + (@datetime.min / 60.0)
+      time -= @datetime.utc_offset / 3600.0
+      jd += time / 24.0
+      t = (jd - 0.5 - Planet::J2000) / 36525.0
+      Planet.get_planets(t, pos, spd)
+      pos[0] = Planet.ascendant(t, time, @lon, @lat)
+      ayn = Planet.get_ayan(t)
+      (0..9).each do |i|
+        tpos[i] = ((pos[i] + ayn) % 360.0 * 60.0).to_i
+        if tpos[i] < 0
+          tpos = tpos
+          n = i
+          tpos[n] += 21600
+        end
+        tsp[i] = (spd[i] * 3600.0).to_i
+      end
+      (0..11).each do |i|
+        (0..9).each do |j|
+          @positions[PLANETS[j]] = i if (tpos[j] / 1800 == i)
+        end
+      end
+      return @positions
+    end
+
+    private
+
+    def validate_values
+      @errors = []
+      @errors << ERRORS[:Date] if @datetime.nil? || !@datetime.is_a?(Time) || @datetime.year > 2300 || @datetime.year < 1600
+      @errors << ERRORS[:Lat] if @lat.nil? || @lat > 90.0 || @lat < -90.0
+      @errors << ERRORS[:Lon] if @lon.nil? || @lon > 180.0 || @lon < -180.0
+      @errors
+    end
+
+  end
 end
-require 'horoscope/horo'
--- spec/horoscope_spec.rb
+++ spec/horoscope_spec.rb
@@ -1,7 +1,53 @@
 require 'spec_helper'
 
 describe Horoscope do
-  it "Nadheems" do
+  it "should error out for blanks" do
+  	h = Horoscope::Horo.new
+  	expect(h.compute).to eql(["Error: Invalid Date. Enter a valid date between years 1600 AD and 2300 AD", 
+  		"Error: Invalid Latitude. Enter between -90.0 to +90.0", 
+  		"Error: Invalid Longitude. Enter between -180.0 to +180.0"])
+  end
+
+  it "should validate an universal horoscope" do
+  	#Sachin Tendulkar's Birth Horoscope
+  	h = Horoscope::Horo.new(:datetime => Time.mktime(1973, 4, 24, 14, 25).getlocal("+05:30"), :lat => 18.60, :lon => -72.50)
+  	h.compute
+  	expect(h.errors).to eql([])
+  	expect(h.positions).to eql({"As"=>4, "Su"=>0, "Mo"=>8, "Ma"=>9, "Me"=>11, "Ju"=>9, "Ve"=>0, "Sa"=>1, "Ra"=>8, "Ke"=>2})
+  end
+
+  it "should validate all dates" do
+  	h = Horoscope::Horo.new(:datetime => nil, :lat => 0, :lon => 0)
+  	expect(h.compute).to eql(["Error: Invalid Date. Enter a valid date between years 1600 AD and 2300 AD"])
+
+  	h.datetime = Time.mktime(1599)
+  	expect(h.compute).to eql(["Error: Invalid Date. Enter a valid date between years 1600 AD and 2300 AD"])
+
+  	h.datetime = Time.mktime(2301)
+  	expect(h.compute).to eql(["Error: Invalid Date. Enter a valid date between years 1600 AD and 2300 AD"])
   	
+  	h.datetime = Time.mktime(2013)
+    h.compute
+  	expect(h.errors).to eql([])
+  end
+
+  it "should validate lat and lon" do
+  	h = Horoscope::Horo.new(:datetime => Time.now)
+  	expect(h.compute).to eql(["Error: Invalid Latitude. Enter between -90.0 to +90.0", 
+  		"Error: Invalid Longitude. Enter between -180.0 to +180.0"])
+
+  	h.lat = +90.1
+  	h.lon = +180.1
+  	expect(h.compute).to eql(["Error: Invalid Latitude. Enter between -90.0 to +90.0", 
+  		"Error: Invalid Longitude. Enter between -180.0 to +180.0"])
+
+  	h.lat = -90.1
+  	h.lon = -180.1
+  	expect(h.compute).to eql(["Error: Invalid Latitude. Enter between -90.0 to +90.0", 
+  		"Error: Invalid Longitude. Enter between -180.0 to +180.0"])
+
+  	h.lat = h.lon = 0
+  	h.compute
+  	expect(h.errors).to eql([])
   end
 end
